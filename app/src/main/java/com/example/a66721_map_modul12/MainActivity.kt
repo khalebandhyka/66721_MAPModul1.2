61db73f1d6b55abadd7a65429f4d72cc802b2376
Khaleb Andhyka Aprijadi <khaleb.andhyka@gmail.com>
2025-02-09 14:30:08 +0700

    Kotak Penelurusam - Pengubah

--- app/src/main/java/com/example/a66721_map_modul12/MainActivity.kt
+++ app/src/main/java/com/example/a66721_map_modul12/MainActivity.kt
@@ -19,6 +19,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.ui.res.stringResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +41,19 @@ fun SearchBar(
     TextField(
         value = "",
         onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedContainerColor = MaterialTheme.colorScheme.surface
+        ),
+        placeholder = {
+            Text(stringResource(R.string.placeholder_search))
+        },
         modifier = modifier
             .fillMaxWidth()
             .heightIn(min = 56.dp)
